2309807f9a8167f69ed81a28bfd9717d094a774e
21321321312 <3123213>
2019-07-10 17:37:24 +0800

    1

--- crawler_job/src/main/java/com/xuexi/job/Application.java
+++ crawler_job/src/main/java/com/xuexi/job/Application.java
@@ -1,6 +1,7 @@
 package com.xuexi.job;
 
 //创建引导类
+//抓取51job招聘信息
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling//开启定时任务
 public class Application {
     public static void main(String[] args) {
-
         SpringApplication.run(Application.class, args);
     }
 }
